95f6adfeb113f34670150a066e9a602fabdc38b2
ibrahimlaskar0 <ibrahimlaskar546@gail.com>
2025-04-19 11:24:48 +0530

    day1of30days

---
+++ Day1/1.py
@@ -0,0 +1,8 @@
+friends = 0
+
+friends += 1
+print(friends)
+
+#friends **= 2 (means 2 is the power)
+#modulus operator % to find the remainder
+
---
+++ Day1/Madlibs.py
@@ -0,0 +1,13 @@
+adjective1 = input("enter an adjective (description): ")
+noun1 = input ("Enter a noun ( person , place, thing): ")
+adjective2 = input("enter an adjective (description): ")
+verb1= input ("ENter a verb ending with 'ing' ")
+adjective3 = input("enter an adjective (description): ")
+
+
+print(f"today i went to a {adjective1} zoo.")
+print(f" in an exhibit , i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f" i was {adjective3}! ")
+
+
