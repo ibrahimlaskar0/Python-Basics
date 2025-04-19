662e161c5cd42afdf8b956da190f8b0ab211028b
ibrahimlaskar0 <ibrahimlaskar546@gail.com>
2025-04-19 11:40:48 +0530

    day1 of 30days part 3

---
+++ Day1/2.py
@@ -0,0 +1,17 @@
+x= 3.14
+y = -5
+z = 7
+
+
+# result = round(x) (round off to the nearest number)
+
+# result = abs (y)  (its the distance of the calue from 0 here the distance is 5)
+
+# result = pow(z,3) ( here the power is 3 and z to the power 3)
+
+# result = max(x,y,z)  ( to find the max value among the 3)
+
+# result = min(x,y,z)  ( to find the min value among the 3)
+
+
+print(result)
---
+++ Day1/3.py
@@ -0,0 +1,14 @@
+import math 
+
+
+
+# print(math.pi)
+
+# print(math.e) (e is the exponential constent)
+
+#result = math.sqrt(9)  ( to find the square root of a number)
+#result= math.ceil(9.1) #( it will round a number up so the ans for this will be 10)
+#result = math.floor(9.1) #( it will round a number down so the ans for this will be 9)
+
+
+print(result)
