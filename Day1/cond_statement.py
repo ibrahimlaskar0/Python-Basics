8ab0c918862374a6abccdbb9f4113d37f194f557
ibrahimlaskar0 <ibrahimlaskar546@gmail.com>
2025-05-06 18:09:48 +0530

    6th may 6 09pm

---
+++ Day1/cond_statement.py
@@ -0,0 +1,23 @@
+num = 5
+print("positive" if num > 0 else "negative")
+
+
+
+num = 6
+result = "even" if num % 2==0 else "odd"
+
+print(result)
+
+
+a =6
+b=7
+
+max_num = a if a>b else b
+min_num = a if a<b else b
+
+
+age = 14
+
+status = "adult" if age>18 else "kid"
+
+print(status)
