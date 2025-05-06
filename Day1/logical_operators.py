b6eb59d035cbaf3917f2f6e4b2eae5005444ab61
ibrahimlaskar0 <ibrahimlaskar546@gmail.com>
2025-05-06 14:33:46 +0530

    6th may 2 33pm

--- Day1/logical_operators.py
+++ Day1/logical_operators.py
@@ -5,3 +5,26 @@
 #not = inverts the condition (not faLse , not true)
 
 temp = 25
+is_raining = False
+
+if temp> 35 or temp <0 or is_raining:
+    print("The outdoor event is cancelled")
+else:
+    print("the outdoor event is still scheduled")
+
+
+#now for and
+
+temp = 25
+is_sunny = True
+
+if temp >=28 and is_sunny:
+    print("It is hot outside")
+    print("it is sunny")
+elif temp <= 0 and is_sunny:
+    print(" it is cold outside")
+    print("it is sunny")
+else:
+    print("the weather is moderate")
+    if is_sunny:
+        print("it is sunny")
