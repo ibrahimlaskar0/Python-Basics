ca92a3cf37d9929b0c60356e3773eeec97f669e3
ibrahimlaskar0 <ibrahimlaskar546@gmail.com>
2025-05-10 23:25:12 +0530

    10th may 11 25 pm

---
+++ All_concepts/lambda_functions.py
@@ -0,0 +1,6 @@
+
+num = input(int("enter the number you want square of "))
+            #takes input
+square = lambda x :x *x  # define lambad function to calculate
+print(square(num))
+
---
+++ All_concepts/loop_dict.py
@@ -0,0 +1,4 @@
+my_dict = {"name": "Ibrahim", "age": 20, "country": "India"}
+
+for key in my_dict:
+    print(key, my_dict[key])
