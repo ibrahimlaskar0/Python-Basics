3c090a90fd69e4f9ca620e971f1b553ddf52780a
ibrahimlaskar0 <ibrahimlaskar546@gmail.com>
2025-05-10 19:24:12 +0530

    10th may 7 24pm

---
+++ All_concepts/string.py
@@ -0,0 +1,9 @@
+text = "hello world"
+
+print (text.lower())   #hello world
+print (text.upper())    #HELLO WORLD
+print (text.strip())   #removes space if there is one at the starting of the sentence 
+print(text.replace("world", "python"))   # replaces the word world with the word python
+
+
+
