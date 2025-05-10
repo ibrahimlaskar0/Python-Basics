278da4972b1bf085c790473f8091b836e5078217
ibrahimlaskar0 <ibrahimlaskar546@gmail.com>
2025-05-10 19:33:56 +0530

    10th may 7 33pm

---
+++ All_concepts/lists.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "banana" , "mango"]
+
+
+print(fruits[0])  # prints the word that we mentioned
+#since python takes 0 as first word so it prints apple
+
+
+#now we will add one more word in the list
+
+fruits.append("orange")
+print(fruits)
+
+#now we will remove one word from the list
+fruits.remove("banana")
+print(fruits)
